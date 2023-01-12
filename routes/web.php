9d76261231111f24b2bc45d56858853f8cdd3a5d
andreadldev <andrea.delisi2@gmail.com>
2023-01-12 15:48:05 +0100

    Aggiungi homepage e pagina chi-siamo

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', '/homepage');
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
+Route::get('/chi-siamo', function () {
+    return view('chi-siamo');
 });
